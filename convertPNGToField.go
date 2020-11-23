c980c8ad592a803f71136b581fe809b2fe24708e
Keftcha <alban.dewilde@gmail.com>
2020-11-23 23:10:21 +0100

    Convert a png file to a Field

---
+++ convertPNGToField.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"strings"
+
+	"github.com/keftcha/floodfill/cell"
+	"github.com/keftcha/floodfill/field"
+)
+
+func convertPNGToField(pngName string) (color.RGBA, field.Field, error) {
+	// Read the PNG file
+	fle, err := ioutil.ReadFile(pngName)
+	if err != nil {
+		return color.RGBA{}, field.Field{}, err
+	}
+
+	img, err := png.Decode(strings.NewReader(string(fle)))
+	if err != nil {
+		return color.RGBA{}, field.Field{}, err
+	}
+
+	cells := make([][]cell.Cell, img.Bounds().Max.Y)
+	initCell := cell.New(0, 0, false)
+	var initColor color.RGBA // init color we return
+	// Travel image pixels
+	for lineIdx := img.Bounds().Min.Y; lineIdx < img.Bounds().Max.Y; lineIdx++ {
+		line := make([]cell.Cell, img.Bounds().Max.X)
+		for columnIdx := img.Bounds().Min.X; columnIdx < img.Bounds().Max.X; columnIdx++ {
+			// Get the color of the pixel
+			c := img.At(columnIdx, lineIdx)
+			changed := c == color.RGBA{0, 0, 0, 255} // Check if it's a border
+
+			// Create the pixel in our cell field
+			line[columnIdx] = cell.New(columnIdx, lineIdx, changed)
+
+			// Check if the cell is different of black or white
+			// That is a start cell of his color
+			if (c != color.RGBA{255, 255, 255, 255} && c != color.RGBA{0, 0, 0, 255}) {
+				initCell.X = columnIdx
+				initCell.Y = lineIdx
+
+				// Initial color
+				r, g, b, a := c.RGBA()
+				initColor = color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+			}
+		}
+		cells[lineIdx] = line
+	}
+
+	field, err := field.New(cells, cells[initCell.Y][initCell.X])
+	return initColor, field, err
+}
--- main.go
+++ main.go
@@ -1,109 +1,19 @@
 package main
 
 import (
-	"image/color"
 	"image/gif"
+	"log"
 	"os"
-
-	"github.com/keftcha/floodfill/cell"
-	"github.com/keftcha/floodfill/field"
 )
 
 func main() {
-	// The field look like this
-	// # # # # # # # # # #
-	// #           ¤     #
-	// #             ¤ ¤ #
-	// #   ¤ ¤ ¤         #
-	// #   ¤   ¤         #
-	// #   ¤   ¤   S     #
-	// #   ¤ ¤           #
-	// #                 #
-	// # # # # # # # # # #
-	// # → border
-	// ¤ → cell that have Changed to true (wall)
-	// S → start cell
-	cells := [][]cell.Cell{
-		[]cell.Cell{
-			cell.New(0, 0, false),
-			cell.New(1, 0, false),
-			cell.New(2, 0, false),
-			cell.New(3, 0, false),
-			cell.New(4, 0, false),
-			cell.New(5, 0, true),
-			cell.New(6, 0, false),
-			cell.New(7, 0, false),
-		},
-		[]cell.Cell{
-			cell.New(0, 1, false),
-			cell.New(1, 1, false),
-			cell.New(2, 1, false),
-			cell.New(3, 1, false),
-			cell.New(4, 1, false),
-			cell.New(5, 1, false),
-			cell.New(6, 1, true),
-			cell.New(7, 1, true),
-		},
-		[]cell.Cell{
-			cell.New(0, 2, false),
-			cell.New(1, 2, true),
-			cell.New(2, 2, true),
-			cell.New(3, 2, true),
-			cell.New(4, 2, false),
-			cell.New(5, 2, false),
-			cell.New(6, 2, false),
-			cell.New(7, 2, false),
-		},
-		[]cell.Cell{
-			cell.New(0, 3, false),
-			cell.New(1, 3, true),
-			cell.New(2, 3, false),
-			cell.New(3, 3, true),
-			cell.New(4, 3, false),
-			cell.New(5, 3, false),
-			cell.New(6, 3, false),
-			cell.New(7, 3, false),
-		},
-		[]cell.Cell{
-			cell.New(0, 4, false),
-			cell.New(1, 4, true),
-			cell.New(2, 4, false),
-			cell.New(3, 4, true),
-			cell.New(4, 4, false),
-			cell.New(5, 4, false),
-			cell.New(6, 4, false),
-			cell.New(7, 4, false),
-		},
-		[]cell.Cell{
-			cell.New(0, 5, false),
-			cell.New(1, 5, true),
-			cell.New(2, 5, true),
-			cell.New(3, 5, false),
-			cell.New(4, 5, false),
-			cell.New(5, 5, false),
-			cell.New(6, 5, false),
-			cell.New(7, 5, false),
-		},
-		[]cell.Cell{
-			cell.New(0, 6, false),
-			cell.New(1, 6, false),
-			cell.New(2, 6, false),
-			cell.New(3, 6, false),
-			cell.New(4, 6, false),
-			cell.New(5, 6, false),
-			cell.New(6, 6, false),
-			cell.New(7, 6, false),
-		},
+	c, f, err := convertPNGToField("src.png")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	f, _ := field.New(cells, cells[4][5])
-
 	// Generate the floodfill gif
-	anim := generateFloodfillGif(
-		f,
-		color.RGBA{0xFF, 0xFF, 0x00, 0x00}, // Yellow
-		5,
-	)
+	anim := generateFloodfillGif(f, c, 10)
 
 	// Create the gif file
 	fle, _ := os.Create("floodfill.gif")
