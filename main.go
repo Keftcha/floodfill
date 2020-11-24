a32a537b8af5304df36ea367495352aaecfd1dca
Keftcha <alban.dewilde@gmail.com>
2020-11-24 20:16:59 +0100

    Add command line arguments
    Command line arguments are for:
    - the input
    - output
    - and the delay between each frame

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"flag"
 	"image/gif"
 	"log"
 	"os"
+
+	"github.com/keftcha/floodfill/field"
 )
 
+var src string
+var out string
+var delay int = 10
+
+func init() {
+	flag.IntVar(&delay, "delay", 10, "Delay between each frame of the gif")
+
+	flag.Parse()
+
+	src = flag.Arg(0)
+	out = flag.Arg(1)
+	if src == "" || out == "" {
+		log.Fatal(
+			"Floodfill usage:\n\n" +
+				"floodfill [--delay int] SOURCE DEST\n\n" +
+				"SOURCE: png image source\n" +
+				"DEST: gif image created",
+		)
+	}
+}
+
 func main() {
-	c, f, err := convertPNGToField("src.png")
+
+	c, f, err := field.ConvertPNGToField(src)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Generate the floodfill gif
-	anim := generateFloodfillGif(f, c, 10)
+	anim := generateFloodfillGif(f, c, delay)
 
 	// Create the gif file
-	fle, _ := os.Create("floodfill.gif")
+	fle, _ := os.Create(out)
 	// Save the gif on disk
 	gif.EncodeAll(fle, &anim)
 }
